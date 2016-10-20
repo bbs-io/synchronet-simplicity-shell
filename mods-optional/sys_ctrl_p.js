7f4954bc9fd37e74bd40b1bb8db63f542f17b1a0
Michael J. Ryan <tracker1@gmail.com>
2016-10-19 17:39:55 -0700

    accound for mods_dir with sys_ctrl+p/u loaders

--- mods-optional/sys_ctrl_p.js
+++ mods-optional/sys_ctrl_p.js
@@ -2,24 +2,27 @@
                  CTRL-P Replacement for Synchronet
 ------------------------------------------------------------------------------
 FILE NAME : sys_ctrl_p.js
-VERSION   : 1.0
+VERSION   : 1.1
 CREATED BY: Michael J. Ryan (tracker1[at]theroughnecks.net)
 CREATED ON: 2003-01-07
+MODIFIED ON: 2016-10-19
 ------------------------------------------------------------------------------
 INSTALLATION:
 	in SCFG->External Programs->Online Hot Key Events
 	Add an event for P
 	command line: ?sys_ctrl_p.js
-	
+
 NOTES:
     if there is a SHELL_CTRL_P.JS (where shell is the user's shell)
     it will run the shell's ctrlp, otherwise, will display the default.
-    
+
     will display a ../text/ctrl_p_header.msg if there is one.
 *****************************************************************************/
 
-var shell_ctrl_p = user.command_shell + "_ctrl_p.js"
+var shell_ctrl_p = user.command_shell.toLowerCase() + "_ctrl_p.js"
 if (file_exists(system.exec_dir + "/" + shell_ctrl_p))
+	bbs.exec("?"+shell_ctrl_p);
+else if (file_exists(system.mods_dir + "/" + shell_ctrl_p))
 	bbs.exec("?"+shell_ctrl_p);
 else {
 	var ctrl_p_header = system.text_dir + "ctrl_p_header.msg";
--- mods-optional/sys_ctrl_u.js
+++ mods-optional/sys_ctrl_u.js
@@ -2,24 +2,27 @@
                  CTRL-U Replacement for Synchronet
 ------------------------------------------------------------------------------
 FILE NAME : sys_ctrl_u.js
-VERSION   : 1.0
+VERSION   : 1.1
 CREATED BY: Michael J. Ryan (tracker1[at]theroughnecks.net)
 CREATED ON: 2003-01-07
+MODIFIED ON: 2016-10-19
 ------------------------------------------------------------------------------
 INSTALLATION:
 	in SCFG->External Programs->Online Hot Key Events
 	Add an event for U
 	command line: ?sys_ctrl_u.js
-	
+
 NOTES:
     if there is a shell_ctrl_u.js (where shell is the user's shell)
     it will run the shell's ctrlp, otherwise, will display the default.
-    
+
     will display a ../text/ctrl_u_header.msg if there is one.
 *****************************************************************************/
 
-var shell_ctrl_u = user.command_shell + "_ctrl_u.js"
+var shell_ctrl_u = user.command_shell.toLowerCase() + "_ctrl_u.js"
 if (file_exists(system.exec_dir + "/" + shell_ctrl_u))
+	bbs.exec("?"+shell_ctrl_u);
+else if (file_exists(system.mods_dir + "/" + shell_ctrl_u))
 	bbs.exec("?"+shell_ctrl_u);
 else {
 	var ctrl_u_header = system.text_dir + "ctrl_u_header.msg";
